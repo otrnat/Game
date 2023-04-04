d7a1925b90bf14f9f25cf977544e8345526cdf83
Natalia Otreshko <32938592+otrnat@users.noreply.github.com>
2023-04-04 21:13:17 +0300

    Change code for correct

--- game_2.py
+++ game_2.py
@@ -8,13 +8,14 @@ symb = "X"
 
 step = 0
 while step <= 9 :
-    row = int(input("Enter your number of row"))
-    column = int(input ("Enter your number of column" ))
+    row = int(input("Enter your number of row: "))
+    column = int(input ("Enter your number of column: " ))
     matrix[row][column] = symb
-    step += 1   
+    step += 1 
+    print(matrix)  
     if symb == "X":
        symb = "0" 
-    if symb == "0":
-       symb = "X"   
+
+    else: symb = "X"   
    
 
