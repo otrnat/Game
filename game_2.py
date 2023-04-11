7069c8a8dc3997dcb8ec46a3d767f6ebb498872f
Natalia Otreshko <32938592+otrnat@users.noreply.github.com>
2023-04-11 21:20:38 +0300

    The logic of Winner for X

--- game_2.py
+++ game_2.py
@@ -1,5 +1,7 @@
-matrix = [[" ", " "," "],[" "," "," "],[" " ," ", " "]]
-
+#           0    1   2
+matrix = [[" ", " "," "], #0
+          [" "," "," "],#  1
+          [" " ," ", " "]]#2
 
 
 symb = "X"
@@ -8,18 +10,48 @@ symb = "X"
 
 step = 0
 while step <= 9 :
-    row = int(input("Enter your number of row: "))
-    column = int(input ("Enter your number of column: " ))
-    matrix[row][column] = symb
-    step += 1
-    for i in range(len(matrix)):
-       print(f"{matrix[i][0]} | {matrix[i][1]} | {matrix[i][2]}")
+   row = int(input("Enter your number of row: "))
+   column = int(input ("Enter your number of column: " ))
+   matrix[row][column] = symb
+   step += 1
 
-    print(matrix)  
-    if symb == "X":
+   for i in range(len(matrix)):
+      print(f"{matrix[i][0]} | {matrix[i][1]} | {matrix[i][2]}")
+
+   print(matrix)  
+   if symb == "X":
        symb = "0" 
-    else: symb = "X" 
+   else: symb = "X" 
+
+   # logic of winner
+   if matrix[0][0] =="X" and matrix[0][1] == "X" and matrix[0][2] == "X":
+      print("You are Winner")
+      break
+   if matrix[0][0] =="X" and matrix[1][0] == "X" and matrix[2][0] == "X":
+      print("You are Winner")
+      break
+   if matrix[2][0] =="X" and matrix[1][1] == "X" and matrix[0][2] == "X":
+      print("You are Winner")
+      break
+   
+   if matrix[0][0] =="X" and matrix[1][1] == "X" and matrix[2][2] == "X":
+      print("You are Winner")
+      break     
+   if matrix[2][0] =="X" and matrix[1][1] == "X" and matrix[0][2] == "X":
+      print("You are Winner")
+      break          
+       
+      
+   if matrix[0][0] =="X" and matrix[1][0] == "X" and matrix[2][0] == "X":
+      print("You are Winner")
+      break    
+   if matrix[0][1] =="X" and matrix[1][1] == "X" and matrix[2][1] == "X":
+      print("You are Winner")
+      break    
+   if matrix[0][2] =="X" and matrix[1][2] == "X" and matrix[2][2] == "X":
+      print("You are Winner")
+      break 
+
+
 
        
-   
-
